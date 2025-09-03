622ed3c8f8a1147416f9358c7f3229a42b5d0c2b
Joe Thomas <156114490+Joethomas0078@users.noreply.github.com>
2025-09-03 10:59:24 +0530

    audiobook.py

--- audiobook/audiobook.py
+++ audiobook/audiobook.py
@@ -7,11 +7,12 @@ engine.setProperty('rate', 125)
 voices = engine.getProperty('voices')
 
 #f
-engine.setProperty('voice', voices[1].id)
+# engine.setProperty('voice', voices[1].id)
 
 #m
-#engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[0].id)
 
 engine.save_to_file(f.read(), 'book.mp3')
 engine.runAndWait()
 engine.stop()
+
